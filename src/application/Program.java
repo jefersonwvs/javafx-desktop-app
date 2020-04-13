eea8e6527c59802ac7ec26cb227c252502cf3fb7
Jeferson Willian Vieira Silva <57540297+jefersonwvs@users.noreply.github.com>
2020-04-13 11:08:07 -0400

    video 265: Tela MainView

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,12 +5,11 @@
  */
 package application;
 
+import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -18,27 +17,19 @@ import javafx.stage.Stage;
  * @author Jeferson Willian
  */
 public class Program extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
-	Button btn = new Button();
-	btn.setText("Say 'Hello World'");
-	btn.setOnAction(new EventHandler<ActionEvent>() {
-	    
-	    @Override
-	    public void handle(ActionEvent event) {
-		System.out.println("Hello World!");
-	    }
-	});
-	
-	StackPane root = new StackPane();
-	root.getChildren().add(btn);
-	
-	Scene scene = new Scene(root, 300, 250);
-	
-	primaryStage.setTitle("Hello World!");
-	primaryStage.setScene(scene);
-	primaryStage.show();
+	try {
+	    FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));	// Obtendo configurações da cena de um arquivo de extensão .fxml
+	    Parent parent = loader.load();		// Objeto principal da view
+	    Scene mainScene = new Scene(parent);	// "Configurando" a cena
+	    primaryStage.setScene(mainScene);		// "Montando" a cena no palco
+	    primaryStage.setTitle("Projeto - Modelo de aplicação desktop genérica");	// Título do palco
+	    primaryStage.show();			// Apresentando o palco
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
     }
 
     /**
@@ -47,5 +38,5 @@ public class Program extends Application {
     public static void main(String[] args) {
 	launch(args);
     }
-    
+
 }

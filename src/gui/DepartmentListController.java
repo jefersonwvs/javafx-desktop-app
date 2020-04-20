90fce477fc41d6030f52fe5aa7dfd2a8f7cce603
Jeferson Willian Vieira Silva <57540297+jefersonwvs@users.noreply.github.com>
2020-04-20 11:07:07 -0400

    vídeo 271: Department service

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -2,7 +2,10 @@ package gui;
 
 import application.Main;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -11,9 +14,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
 
+    private DepartmentService service;	// associação
+    
     @FXML
     private TableView<Department> tableViewDepartment;
     
@@ -26,9 +32,16 @@ public class DepartmentListController implements Initializable {
     @FXML
     private Button btNew;
     
+    /* Tipo de lista que pode ser visualizável em elementos do JavaFX*/
+    private ObservableList<Department> obsList;
+    
     @FXML
     private void onBtNewAction() {
 	System.out.println("onBtNewAction");
+    }
+    
+    public void setDepartmentService(DepartmentService service) { //injenção de dependêcia
+	this.service = service;
     }
     
     @Override
@@ -38,12 +51,22 @@ public class DepartmentListController implements Initializable {
 
     private void initializeNodes() {
 	/* Para que as colunas funcionem é preciso associá-las aos respectivos atributos */
-	tableColumnId.setCellFactory(new PropertyValueFactory("id"));
-	tableColumnName.setCellFactory(new PropertyValueFactory("name"));
+	tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+	tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
 	
 	/* Macete para redimensionar a altura da tabela conforme a janela */
 	Stage stage = (Stage) Main.getMainScene().getWindow();
 	tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	
+    }
+    
+    /* Método que atualiza a tabela de departamentos */
+    public void updateTableView() {
+	if (service == null) { //Precaução para caso o programador tenha esquecido de injetar a dependência
+	    throw new IllegalStateException("Service was null");
+	}
+	List<Department> list = service.findAll();	    // recebendo a lista de objetos
+	obsList = FXCollections.observableArrayList(list);  // transformando em um tipo visualizável
+	tableViewDepartment.setItems(obsList);		    // carregando na tabela
     }
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
@@ -33,7 +34,7 @@ public class MainViewController implements Initializable {
     
     @FXML
     public void onMenuItemDepartmentAction() {
-	loadView("/gui/DepartmentList.fxml");
+	loadView2("/gui/DepartmentList.fxml");
     }
     
     @FXML
@@ -63,6 +64,34 @@ public class MainViewController implements Initializable {
 	    mainVBox.getChildren().clear();
 	    mainVBox.getChildren().add(mainMenu);
 	    mainVBox.getChildren().addAll(newVBox.getChildren());
+	    
+	} catch (IOException ex) {
+	    Alerts.showAlert("IO Exception", "Erro ao carregar view", ex.getMessage(), Alert.AlertType.ERROR);
+	}
+    }
+    
+    private synchronized void loadView2(String absoluteName) {
+	try {
+	    /* Carregando o "desenho" (.fxml) em um objeto para o Java */
+	    FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+	    VBox newVBox = loader.load();
+	    
+	    /* Obtendo a referência da minha view principal */
+	    Scene mainScene = Main.getMainScene();
+	    
+	    /* A partir da mainScene, por meio de métodos específicos e upcastings obtemos a referência para o objeto onde será anexado o newVBox*/
+	    VBox mainVBox = (VBox) (((ScrollPane) mainScene.getRoot())	// .getRoot(): pega a referência para o objeto principal, que neste caso trata-se de um ScrollPane e, por isso, foi necessário fazer upcasting
+			    .getContent());				// .getContent(): pega a ref. para o conteúdo do ScrollPane
+	    
+	    /* Para colocar os filhos do newVBox no mainVBox, primeiro guardamos os filhos na mainVBox que queremos manter, depois limpamos e recolocamos os de interesse*/
+	    Node mainMenu = mainVBox.getChildren().get(0);
+	    mainVBox.getChildren().clear();
+	    mainVBox.getChildren().add(mainMenu);
+	    mainVBox.getChildren().addAll(newVBox.getChildren());
+	    
+	    DepartmentListController controller = loader.getController();
+	    controller.setDepartmentService(new DepartmentService());
+	    controller.updateTableView();
 	    
 	} catch (IOException ex) {
 	    Alerts.showAlert("IO Exception", "Erro ao carregar view", ex.getMessage(), Alert.AlertType.ERROR);

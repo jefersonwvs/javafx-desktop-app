f3d0a8c184137455de0d6db1c59504ca1ba48c50
Jeferson Willian Vieira Silva <57540297+jefersonwvs@users.noreply.github.com>
2020-04-21 11:17:55 -0400

    vídeo 274: DepartmentForm design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,17 +1,26 @@
 package gui;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -36,8 +45,9 @@ public class DepartmentListController implements Initializable {
     private ObservableList<Department> obsList;
     
     @FXML
-    private void onBtNewAction() {
-	System.out.println("onBtNewAction");
+    private void onBtNewAction(ActionEvent event) {
+	Stage parentStage = Utils.currentStage(event);	// referência para o palco pai do próximo evento
+	createDialogForm("/gui/DepartmentForm.fxml", parentStage);
     }
     
     public void setDepartmentService(DepartmentService service) { //injenção de dependêcia
@@ -68,5 +78,24 @@ public class DepartmentListController implements Initializable {
 	List<Department> list = service.findAll();	    // recebendo a lista de objetos
 	obsList = FXCollections.observableArrayList(list);  // transformando em um tipo visualizável
 	tableViewDepartment.setItems(obsList);		    // carregando na tabela
+    }
+    
+    private void createDialogForm(String absoluteName, Stage parentStage) {
+	try {
+	    FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+	    Pane pane = loader.load();	//carrega um novo painel
+	    
+	    /* Como se trata de janelas de diálogo, uma vez que são modais, elas devem ocorrer por cima de outras*/
+	    Stage dialogStage = new Stage();	// instanciamento de um novo palco
+	    dialogStage.setTitle("Insira os dados do novo departamento");
+	    dialogStage.setScene(new Scene(pane));  // configurando a cena com o painel desejado
+	    dialogStage.setResizable(false);	    // palco não redimensionável
+	    dialogStage.initOwner(parentStage);	    // pai da janela, janela anterior
+	    dialogStage.initModality(Modality.WINDOW_MODAL); //janela travada até ser fechada salvando ou cancelando
+	    dialogStage.showAndWait();	//mostra o palco e aguarda
+	}
+	catch (IOException e){
+	    Alerts.showAlert("IO Exception", "Erro ao carregar view", e.getMessage(), Alert.AlertType.ERROR);
+	}
     }
 }
